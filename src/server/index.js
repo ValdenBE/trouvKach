96154277a9db4292df0cab86b469ef9f7258cc43
LVannebenne <lindsay.vannebenne@gmail.com>
2019-09-11 10:12:05 +0200

    2 dummy routes api/bank and api/terminal

--- src/server/index.js
+++ src/server/index.js
@@ -8,16 +8,10 @@
 
 import express from "express";
 import path from "path";
-const MongoClient = require("mongodb").MongoClient;
-const assert = require("assert");
-const url = "mongodb://dev:dev@mongo:27017";
-const dbName = "trouvkash";
-const client = new MongoClient(url);
 
 const {APP_PORT} = process.env;
 
 const app = express();
-const router = require("express").Router();
 
 app.use(express.static(path.resolve(__dirname, "../../bin/client")));
 
@@ -30,14 +24,59 @@ app.listen(APP_PORT, () =>
     console.log(`🚀 Server is listening on port ${APP_PORT}.`),
 );
 
-router.get("/", (req, res) => {
+app.get("/api/bank", (req, res) => {
     res.json({
-        status: "API is working NOWWWW",
-        message: "Welcome to TrouvKach",
+        0: {
+            _id: "53937660e0b8c05979c6ea55",
+            country: "BE",
+            color: "4a961d",
+            name: "Argenta",
+            icon: "argenta.png",
+            url: "http://www.argenta.be",
+            created_at: "2013-06-06 20:40:38",
+            updated_at: "2013-06-06 20:40:38",
+            deleted_at: null,
+        },
+        1: {
+            _id: "53937660e0b8c05979c6ea56",
+            country: "BE",
+            color: "0b2c81",
+            name: "AXA",
+            icon: "axa.png",
+            url: "http://www.axa.be",
+            created_at: "2013-06-06 20:40:38",
+            updated_at: "2013-06-06 20:40:38",
+            deleted_at: null,
+        },
     });
 });
 
-app.get("/bank", (req, res) => {
+app.get("/api/terminal", (req, res) => {
+    res.json({
+        0: {
+            _id: "5393803ce0b8c05979c6ead6",
+            bank: "53937660e0b8c05979c6ea55",
+            latitude: 50.6802,
+            longitude: 5.548,
+            address: "Rue François-Lefèbvre 79, 4000 Liege",
+            created_at: "2013-06-10 18:43:44",
+            updated_at: "2013-06-10 18:43:44",
+            deleted_at: null,
+        },
+        1: {
+            _id: "5393803ce0b8c05979c6ead7",
+            bank: "53937660e0b8c05979c6ea55",
+            latitude: 50.6233,
+            longitude: 5.5712,
+            address: "Rue Auguste Buisseret 1, 4000 Liege",
+            created_at: "2013-06-10 18:43:44",
+            updated_at: "2013-06-10 18:43:44",
+            deleted_at: null,
+        },
+    });
+});
+
+/*app.get("/bank", (req, res) => {
     client.connect(err => {
         assert.equal(null, err);
         if (err == null) {
@@ -58,6 +97,6 @@ app.get("/bank", (req, res) => {
             console.error(err);
         }
     });
-});
+});*/
 
 //app.use("/api", router);
