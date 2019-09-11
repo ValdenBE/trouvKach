cb458273c6b4f3fb9f97d2119866d9531a4b59a0
ValdenBE <pirghaye.f@gmail.com>
2019-09-11 12:14:25 +0200

    axios api

--- src/client/components/list/list.js
+++ src/client/components/list/list.js
@@ -9,8 +9,27 @@
 import React, {Component} from "react";
 import Object from "./object";
 import MainMap from "../map/main-map";
+import axios from "axios";
 
 export default class List extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            atmData: [],
+        };
+    }
+    componentDidMount() {
+        axios
+            .get("http://localhost/api/terminal")
+            .then(response => {
+                this.setState({atmData: response.data});
+                console.log(this.state.atmData);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
     render() {
         if (this.props.viewList) {
             return (
