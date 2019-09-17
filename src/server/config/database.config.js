90c22a2572263e75672085ff6f185a3dfd1b5796
LVannebenne <lindsay.vannebenne@gmail.com>
2019-09-17 12:10:19 +0200

    deploy v7 + DB

--- src/server/config/database.config.js
+++ src/server/config/database.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-    url: "mongodb+srv://dev:dev@cluster0-p8gwr.mongodb.net/test",
+    url:
+        "mongodb+srv://dev:dev@cluster0-p8gwr.mongodb.net/test?retryWrites=true&w=majority",
 };
 //"mongodb://mongo:27017/admin"
